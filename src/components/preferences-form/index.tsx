fe2506d63ed3498acf4cba52b66bf40984aaa6ee
Adrian Lafond <mail@adrianlafond.com>
2022-04-09 15:16:28 -0400

    use focus-visible

--- src/components/preferences-form/index.tsx
+++ src/components/preferences-form/index.tsx
@@ -2,14 +2,20 @@ import { FunctionalComponent, h } from 'preact';
 import { useContext } from 'preact/hooks';
 import { route } from 'preact-router';
 import classnames from 'classnames';
-import page from '../shared/page.scss';
-import style from './style.scss';
 import { NavigationContext, Preferences, PreferencesContext } from '../../contexts';
 import { NavigationButton } from '../navigation-button';
+import page from '../shared/page.scss';
+import typography from '../shared/typography.scss';
+import style from './style.scss';
 
 export const PreferencesForm: FunctionalComponent = () => {
   const { path, forecastPath } = useContext(NavigationContext);
   const { preferences, update } = useContext(PreferencesContext);
+
+  // Prevents global spatial navigation panning.
+  const onDown = (event: Event) => {
+    event.stopPropagation();
+  };
 
   const handleForecastsClick = () => {
     route(forecastPath);
@@ -26,10 +32,10 @@ export const PreferencesForm: FunctionalComponent = () => {
   return (
     <div class={classnames(page.page, style.preferences)}>
       <div class={page.page__content}>
-        <h2>Preferences</h2>
+        <h2 class={typography.h2}>Preferences</h2>
 
         <label>Temperature unit</label>
-        <select onChange={handleTempUnitChange}>
+        <select onChange={handleTempUnitChange} onMouseDown={onDown} onTouchStart={onDown}>
           <option value="C" selected={preferences.temperatureUnit === 'C'}>celcius</option>
           <option value="F" selected={preferences.temperatureUnit === 'F'}>fahrenheit</option>
         </select>
