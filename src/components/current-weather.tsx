2410b0946133cfda55caf8ccf2b4428d8938c547
Adrian Lafond <mail@adrianlafond.com>
2022-09-18 08:44:04 -0400

    show feels like for current weather

--- src/components/current-weather.tsx
+++ src/components/current-weather.tsx
@@ -4,20 +4,40 @@ import { RootState } from '../store'
 import { WeatherCode } from '../constants'
 
 export const CurrentWeather = () => {
-  const { temperatureUnit } = useAppSelector((state: RootState) => state.forecast)
-  const { time, temperature, weatherCode } = useAppSelector((state: RootState) => state.forecast.currentWeather)
   const { showForecast } = useAppSelector((state: RootState) => state.ui)
 
-  return showForecast
-    ? (
-      <div>
-        <h3 class="text-9xl">
-          {isNaN(temperature) ? '-' : temperature} {temperatureUnit}&deg;
-        </h3>
-        <p>
-          {new Date(time).toLocaleString()} / {WeatherCode[weatherCode]}
-        </p>
-      </div>
-      )
-    : null
+  if (!showForecast) {
+    return null
+  }
+
+  const { temperatureUnit } = useAppSelector((state: RootState) => state.forecast)
+  const { time } = useAppSelector((state: RootState) => state.forecast.currentWeather)
+  const {
+    temperature,
+    apparentTemperature,
+    weatherCode
+  } = useAppSelector((state: RootState) => {
+    const currentHourly = state.forecast.hourly.find(item => item.time === time)
+    return currentHourly != null
+      ? currentHourly
+      : {
+          temperature: '-',
+          apparentTemperature: '-',
+          weatherCode: 0
+        }
+  })
+
+  return (
+    <div>
+      <h3 class="text-9xl">
+        {temperature} {temperatureUnit}&deg;
+      </h3>
+      <p>
+        Feels like: {apparentTemperature} {temperatureUnit}&deg;
+      </p>
+      <p>
+        {new Date(time).toLocaleString()} / {WeatherCode[weatherCode]}
+      </p>
+    </div>
+  )
 }
